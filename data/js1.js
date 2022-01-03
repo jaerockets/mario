4e7bc3d8776860974e373e0e5c3025db061f5498
Jae Aboleth <jae.rockets@gmail.com>
2022-01-03 03:01:53 -0800

    the

--- data/js1.js
+++ data/js1.js
@@ -9,15 +9,39 @@ window.resizeTo(300, 600)
 // remember 40 unit canvas buffer
 
 ctx.fillStyle = '#00FF00'
-ctx.fillRect(0, 525, 300, 1)
+ctx.fillRect(185, 0, 100, 100)
+ctx.fillRect(0, 525, 100, 1)
+
 ctx.fillStyle = '#ffffff'
 ctx.font = "11px consolas"
 ctx.fillText('bread man killed you', 0, 536)
 
-const URL = 'https://sean.fish/mal_unapproved/anime';
+ctx.fillStyle = '#000000'
+ctx.fillRect(187, 3, 95, 95)
+
+/* const URL = 'https://sean.fish/mal_unapproved/anime';
 fetch(URL)
 .then(res => res.text())
 .then(text => {
     console.log(text);
 })
-.catch(err => console.log(err));
+.catch(err => console.log(err)); */
+
+
+//copied from squaremenu
+
+var mouseX = 0
+var mouseY = 0
+
+function coordCheck(event) { //sets mousePos coords to read-only mousePos values
+    mouseX = event.pageX
+    mouseY = event.pageY
+    if (mouseX > 187) {
+        if (mouseY < 100) {
+            console.log('i eat weed')
+        }
+    }
+}
+
+canvas.addEventListener("mousemove", coordCheck, false); //runs coordCheck ;132 on mousemove event
+canvas.addEventListener("mouseup", coordCheck, false) //runs mouseRelease ;128 on mouseup event
